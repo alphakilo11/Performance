d25899b639ed130863b10ea4e7d901b70bd6ffee
alphakilo11 <83025486+alphakilo11@users.noreply.github.com>
2023-03-02 05:53:27 +0100

    Create nth_prime.py

---
+++ prime_numbers/nth_prime.py
@@ -0,0 +1,29 @@
+from timeit import default_timer as timer
+
+def nth_prime(n):
+    """
+    This function takes an integer n and returns the nth prime number.
+    Source: ChatGPT Feb 13 Version
+    """
+    if n <= 0:
+        return None
+    primes = [2]
+    num = 3
+    while len(primes) < n:
+        is_prime = True
+        sqrt_num = int(num ** 0.5) + 1
+        for p in primes:
+            if p > sqrt_num:
+                break
+            if num % p == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(num)
+        num += 2
+    return primes[-1]
+  
+start_time = timer()
+for i in range(10):
+  print(nth_prime(50000))
+print(f'Dauer: {timer() - start_time}')
